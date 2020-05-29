79375b0063ef8fb84f2265e9a274346b3c533e33
ash <aichiko66@163.com>
2020-05-29 20:46:27 +0800

    Initial commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('title'),
+      ),
+      child: Center(
+        child: Text('123'),
+      ),
+    );
+  }
+}
